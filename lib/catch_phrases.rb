76692c544f7039cba0d10e705f2ed1164fe1dda6
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-07 22:50:49 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,9 @@
+phrase = "It's-a me, Mario!"
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
-phrase = "It's-a me, Mario!"
+
 def toadstool
   puts status
 end

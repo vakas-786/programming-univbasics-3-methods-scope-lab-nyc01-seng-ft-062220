da47bbdde1e8ca065c7d26d676b3e0073edc9d16
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-07 23:09:26 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -18,6 +18,5 @@ def all_phrases
   phrase = "It's-a me, Mario!"
   phrase = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's Dangerous To Go Alone! Take This."
-
   puts phrase
 end
